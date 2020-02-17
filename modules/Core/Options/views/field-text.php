3e85caaf3fb1feb55b092c376266750005b6a8aa
Stephan Langenegger <stephan@faktorvier.ch>
2020-02-17 17:58:16 +0100

    Add wp class to text field

--- modules/Core/Options/views/field-text.php
+++ modules/Core/Options/views/field-text.php
@@ -9,5 +9,6 @@
 	name="<?php echo F4_EP_CORE_OPTION_NAME; ?>[<?php echo $field_name; ?>]"
 	id="<?php echo F4_EP_CORE_OPTION_NAME . $field_name; ?>"
 	value="<?php echo $options[$field_name]; ?>"
+	class="regular-text"
 	placeholder="<?php echo $field['placeholder']; ?>"
 />
